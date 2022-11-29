3b5e2bee30927bd8cf36ebf7c3b058d8415a1d74
Geoorge <igeoorge@gmail.com>
2022-11-29 00:49:37 -0300

    Add project files.

---
+++ PcStudio/Pc.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcStudio
+{
+    internal class Pc
+    {
+        public string Name { get; set; }
+        public List<Component> Components { get; set; } = new List<Component>();
+    }
+
+    public class Component
+    {
+        public string Name { get; set; }
+        public string? Brand { get; set; }
+        public ComponentType Type { get; set; }
+
+        public Component(string name, ComponentType type)
+        {
+            Name = name;
+            Type = type;
+
+        }
+        public Component(string name, ComponentType type, string brand) : this(name, type)
+        {
+            Brand = brand;
+        }
+
+
+        public static List<Component> All = new List<Component>()
+        {
+            new Component("Ryzen 7 5700X", ComponentType.Cpu, "AMD"),
+            new Component("Intel i5 12400", ComponentType.Cpu, "Intel"),
+            new Component("X570", ComponentType.Motherboard, "AMD"),
+            new Component("Z850", ComponentType.Motherboard, "Intel"),
+            new Component("GSkill 32gb 4400mhz", ComponentType.Ram),
+            new Component("RTX 3090", ComponentType.Gpu),
+            new Component("Gigabyte 850W 80+ Platinum", ComponentType.PowerSupply),
+            new Component("Thermaltake H330TG", ComponentType.Case),
+
+        };
+
+
+
+        public enum ComponentType
+        {
+            None = 0,
+            Case = 1,
+            Cpu = 2,
+            Gpu = 3,
+            Motherboard = 4,
+            PowerSupply = 5,
+            Ram = 6
+        }
+    }
+}
---
+++ PcStudio/Program.cs
@@ -0,0 +1,110 @@
+﻿using PcStudio;
+using System.Text;
+using static PcStudio.Component;
+
+Console.WriteLine("Welcome, select an option from main Menu");
+DisplayMenuOptions(0);
+
+void DisplayMenuOptions(int menuOption)
+{
+    bool loop = true;
+    Pc? currentPc = null;
+
+    while (loop)
+    {
+
+        Console.WriteLine(@"
+0. Quit
+1. Create PC
+2. Our Recomndations
+3. List of CPUs
+4. List of GPUs
+5. List Current PC
+");
+        var selectedMenuOption = int.Parse(Console.ReadLine()!);
+        switch (selectedMenuOption)
+        {
+            case 0:
+                loop = false;
+                break;
+            case 1:
+                currentPc = CreatePcMenuActions();
+                break;
+            case 2:
+
+                break;
+            case 5:
+                if (currentPc is null)
+                {
+                    Console.WriteLine("Empty PC");
+                    return;
+                }
+
+                foreach (var component in currentPc.Components)
+                {
+                    Console.WriteLine($"{component.Name}");
+                }
+
+                break;
+        }
+    }
+}
+
+Pc CreatePcMenuActions()
+{
+    bool loop = true;
+    var currentPC = new Pc();
+    var sb = new StringBuilder();
+
+    sb.AppendLine("0. Back");
+    foreach (var componentType in Enum.GetValues<ComponentType>())
+    {
+        if (componentType == ComponentType.None)
+            continue;
+
+        sb.AppendLine($"{(int)componentType}. Add {componentType.ToString()}");
+    }
+
+    while (loop)
+    {
+        Console.WriteLine(sb.ToString());
+        var selectedMenuOption = int.Parse(Console.ReadLine()!);
+
+        if (Enum.TryParse<ComponentType>(selectedMenuOption.ToString(), out ComponentType selectedComponentType))
+        {
+            if (selectedComponentType == ComponentType.None)
+            {
+                loop = false;
+                continue;
+            }
+
+            var availableComponents = GetComponents().Where(e => e.Type == selectedComponentType);
+
+            var currentCpu = currentPC.Components.FirstOrDefault(e => e.Type == ComponentType.Cpu);
+            if (currentCpu != null)
+            {
+                availableComponents = availableComponents.Where(e => e.Brand == currentCpu.Brand);
+            }
+
+            var selectedComponent = PrintAndSelectAvailableComponents(availableComponents.ToList());
+            currentPC.Components.Add(selectedComponent);
+        }
+    }
+    return currentPC;
+}
+
+
+List<Component> GetComponents()
+{
+    return Component.All;
+}
+
+Component PrintAndSelectAvailableComponents(List<Component> availableComponents)
+{
+    for (int i = 0; i < availableComponents.Count; i++)
+    {
+        Console.WriteLine($"{i}. {availableComponents[i].Name}");
+    }
+    var selected = int.Parse(Console.ReadLine());
+    return availableComponents[selected];
+}
